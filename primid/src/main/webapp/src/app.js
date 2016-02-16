01bc1b8122840d4d376aaa06d2bd8cf09d29f6d5
Luis Mesa <la.mesa10@uniandes.edu.co>
2016-02-16 10:43:23 -0500

    Estructuración final con ui router, ya funcionan las vistas :D

--- primid/src/main/webapp/src/app.js
+++ primid/src/main/webapp/src/app.js
@@ -7,32 +7,36 @@
         }]);
 
     mod.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
-            $urlRouterProvider.otherwise("/Viajero");
+            $urlRouterProvider.otherwise("/vista1");
             $stateProvider
 
+                    .state('vista1', {
+                        url: '/vista1',
+                        views:{ "idViajero":{templateUrl:"src/modules/Viajero/Viajero.tpl.html"}, "idEvento":{templateUrl:"src/modules/evento/evento.tpl.html"}, "idItinerario":{templateUrl:"src/modules/Itinerario/itinerario.tpl.html"}, "idMultimedia":{templateUrl:"src/modules/Multimedia/multimedia.tpl.html"}, "idTimeLine":{templateUrl:"src/modules/Timeline/timeline.tpl.html"}, "idMapa":{templateUrl:"src/modules/Mapa/mapa.tpl.html"}
+                    }})
                     .state('Viajero', {
                         url: '/Viajero',
-                        templateUrl: "resources/src/modules/User/Viajero.tpl.html"
+                        templateUrl: "src/modules/User/Viajero.tpl.html"
                     })
                     .state('evento', {
                         url: '/evento',
-                        templateUrl: "resources/src/modules/evento/evento.tpl.html"
+                        templateUrl: "src/modules/evento/evento.tpl.html"
                     })
                     .state('Itinerario1', {
                         url: '/Itinerario1',
-                        templateUrl: "resources/src/modules/Itinerario/itinerario1.tpl.html"
+                        templateUrl: "src/modules/Itinerario/itinerario1.tpl.html"
                     })
                     .state('Itinerario2', {
                         url: '/Itinerario2',
-                        templateUrl: "resources/src/modules/Itinerario/itinerario2.tpl.html"
+                        templateUrl: "src/modules/Itinerario/itinerario2.tpl.html"
                     })
                     .state('Multimedia', {
                         url: '/Multimedia',
-                        templateUrl: "resources/src/modules/Multimedia/multimedia.tpl.html"
+                        templateUrl: "src/modules/Multimedia/multimedia.tpl.html"
                     })
                     .state('Timeline', {
                         url: '/Timeline',
-                        templateUrl: "resources/src/modules/Timeline/timeline.tpl.html"
+                        templateUrl: "src/modules/Timeline/timeline.tpl.html"
                     });
         }]);
 })(window.angular);
