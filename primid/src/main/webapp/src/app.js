0fbc17a63ed4908ac19f85741cd6392ee40b240b
karen osorio <kl.osorio10@uniandes.edu.co>
2016-02-25 20:20:45 -0500

    Parcial - Evento

--- primid/src/main/webapp/src/app.js
+++ primid/src/main/webapp/src/app.js
@@ -12,7 +12,7 @@
 
                     .state('vista1', {
                         url: '/vista1',
-                        views:{ "idViajero":{templateUrl:"src/modules/Viajero/Viajero.tpl.html"}, "idEvento":{templateUrl:"src/modules/evento/evento.tpl.html"}, "idItinerario":{templateUrl:"src/modules/Itinerario/itinerario.tpl.html"}, "idMultimedia":{templateUrl:"src/modules/Multimedia/multimedia.tpl.html"}, "idTimeLine":{templateUrl:"src/modules/Timeline/timeline.tpl.html"}, "idMapa":{templateUrl:"src/modules/Mapa/mapa.tpl.html"}
+                        views:{ "idViajero":{templateUrl:"src/modules/Viajero/Viajero.tpl.html"}, "idEvento":{templateUrl:"src/modules/evento/evento.tpl.html", controller:"eventoCtrl"}, "idItinerario":{templateUrl:"src/modules/Itinerario/itinerario.tpl.html"}, "idMultimedia":{templateUrl:"src/modules/Multimedia/multimedia.tpl.html"}, "idTimeLine":{templateUrl:"src/modules/Timeline/timeline.tpl.html"}, "idMapa":{templateUrl:"src/modules/Mapa/mapa.tpl.html"}
                     }})
                     .state('Viajero', {
                         url: '/Viajero',
