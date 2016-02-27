a4d9805772a595f277dd7515d9e7e66924c72fd0
JDTawa <jd.torres11@uniandes.edu.co>
2016-02-27 15:31:59 -0500

    Reestructuración del proyecto, basado en ejemploBook

---
+++ primid/src/main/webapp/src/modules/viajero/viajero.ctrl.js
@@ -0,0 +1,122 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+(function (ng) {
+
+
+    var mod = ng.module("reviewModule");
+
+    mod.controller("reviewCtrl", ["$scope", "reviewService", function ($scope, svc) {
+
+            $scope.currentRecord = {};
+            $scope.records = [];
+
+            $scope.today = function () {
+                $scope.value = new Date();
+            };
+
+            $scope.clear = function () {
+                $scope.value = null;
+            };
+
+            $scope.open = function ($event) {
+                $event.preventDefault();
+                $event.stopPropagation();
+
+                $scope.opened = true;
+            };
+
+            //Variables para el controlador
+            this.readOnly = false;
+            this.editMode = false;
+
+            var self = this;
+            this.changeTab = function (tab) {
+                $scope.tab = tab;
+            };
+
+           /*
+             * Funcion createRecord emite un evento a los $scope hijos del controlador por medio de la
+             * sentencia &broadcast ("nombre del evento", record), esto con el fin cargar la información de modulos hijos
+             * al actual modulo.
+             * Habilita el modo de edicion. El template de la lista cambia por el formulario.
+             *
+             */
+
+            this.createRecord = function () {
+                $scope.$broadcast("pre-create", $scope.currentRecord);
+                this.editMode = true;
+                $scope.currentRecord = {};
+                $scope.$broadcast("post-create", $scope.currentRecord);
+            };
+
+            /*
+             * Funcion editRecord emite el evento ("pre-edit") a los $Scope hijos del controlador por medio de la
+             * sentencia &broadcast ("nombre del evento", record), esto con el fin cargar la información de modulos hijos
+             * al actual modulo.
+             * Habilita el modo de edicion.  Carga el template de formulario con los datos del record a editar.
+             *
+             */
+
+            this.editRecord = function (record) {
+                $scope.$broadcast("pre-edit", $scope.currentRecord);
+                return svc.fetchRecord(record.id).then(function (response) {
+                    $scope.currentRecord = response.data;
+                    self.editMode = true;
+                    $scope.$broadcast("post-edit", $scope.currentRecord);
+                    return response;
+                });
+            };
+
+            /*
+             * Funcion fetchRecords consulta el servicio svc.fetchRecords con el fin de consultar
+             * todos los registros del modulo review.
+             * Guarda los registros en la variable $scope.records
+             * Muestra el template de la lista de records.
+             */
+
+            this.fetchRecords = function () {
+                return svc.fetchRecords().then(function (response) {
+                    $scope.records = response.data;
+                    $scope.currentRecord = {};
+                    self.editMode = false;
+                    return response;
+                });
+            };
+
+            /*
+             * Funcion saveRecord hace un llamado al servicio svc.saveRecord con el fin de
+             * persistirlo en base de datos.
+             * Muestra el template de la lista de records al finalizar la operación saveRecord
+             */
+            this.saveRecord = function () {
+                return svc.saveRecord($scope.currentRecord).then(function () {
+                    self.fetchRecords();
+
+                });
+            };
+
+            /*
+             * Funcion deleteRecord hace un llamado al servicio svc.deleteRecord con el fin
+             * de eliminar el registro asociado.
+             * Muestra el template de la lista de records al finalizar el borrado del registro.
+             */
+            this.deleteRecord = function (record) {
+                return svc.deleteRecord(record.id).then(function () {
+                    self.fetchRecords();
+                });
+            };
+
+            /*
+             * Funcion fetchRecords consulta todos los registros del módulo editorial en base de datos
+             * para desplegarlo en el template de la lista.
+             */
+            this.fetchRecords();
+
+        }]);
+
+})(window.angular);
