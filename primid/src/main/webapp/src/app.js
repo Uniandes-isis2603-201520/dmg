7eeee27e008f75c56ed83b41865c767f2f1b8b40
JDTawa <jd.torres11@uniandes.edu.co>
2016-02-27 13:24:58 -0500

    Reestructuración del proyecto, basado en ejemploBook

--- primid/src/main/webapp/src/app.js
+++ primid/src/main/webapp/src/app.js
@@ -7,36 +7,51 @@
         }]);
 
     mod.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
-            $urlRouterProvider.otherwise("/vista1");
+
+            $urlRouterProvider.otherwise("/section-home");
             $stateProvider
 
-                    .state('vista1', {
-                        url: '/vista1',
-                        views:{ "idViajero":{templateUrl:"src/modules/Viajero/Viajero.tpl.html"}, "idEvento":{templateUrl:"src/modules/evento/evento.tpl.html", controller:"eventoCtrl"}, "idItinerario":{templateUrl:"src/modules/Itinerario/itinerario.tpl.html"}, "idMultimedia":{templateUrl:"src/modules/Multimedia/multimedia.tpl.html"}, "idTimeLine":{templateUrl:"src/modules/Timeline/timeline.tpl.html"}, "idMapa":{templateUrl:"src/modules/Mapa/mapa.tpl.html"}
-                    }})
-                    .state('Viajero', {
-                        url: '/Viajero',
-                        templateUrl: "src/modules/User/Viajero.tpl.html"
+                    .state('section-home', {
+                        url: '/section-home',
+                        //controller: "section-homeCtrl",
+                        //controllerAs: "ctrl",
+                        templateUrl: "src/modules/section-H/section-home.tpl.html"
+                    })
+                     .state('multimedia', {
+                        url: '/multimedia',
+                        //controller: "section-homeCtrl",
+                        //controllerAs: "ctrl",
+                        templateUrl: "src/modules/multimedia/multimedia.tpl.html"
+                    })
+                     .state('itinerario', {
+                        url: '/itinerario',
+                        //controller: "section-homeCtrl",
+                        //controllerAs: "ctrl",
+                        templateUrl: "src/modules/Itinerario/itinerario1.tpl.html"
+                    })
+                    .state('mapa', {
+                        url: '/mapa',
+                        //controller: "section-homeCtrl",
+                        //controllerAs: "ctrl",
+                        templateUrl: "src/modules/mapa/mapa.tpl.html"
+                    })
+                    .state('viajero', {
+                        url: '/viajero',
+                        //controller: "section-homeCtrl",
+                        //controllerAs: "ctrl",
+                        templateUrl: "src/modules/viajero/viajero.tpl.html"
+                    })
+                    .state('timeline', {
+                        url: '/timeline',
+                        //controller: "section-homeCtrl",
+                        //controllerAs: "ctrl",
+                        templateUrl: "src/modules/Timeline/timeline2.tpl.html"
                     })
                     .state('evento', {
                         url: '/evento',
+                        //controller: "eventoCtrl",
+                        //controllerAs: "ctrl",
                         templateUrl: "src/modules/evento/evento.tpl.html"
-                    })
-                    .state('Itinerario1', {
-                        url: '/Itinerario1',
-                        templateUrl: "src/modules/Itinerario/itinerario1.tpl.html"
-                    })
-                    .state('Itinerario2', {
-                        url: '/Itinerario2',
-                        templateUrl: "src/modules/Itinerario/itinerario2.tpl.html"
-                    })
-                    .state('Multimedia', {
-                        url: '/Multimedia',
-                        templateUrl: "src/modules/Multimedia/multimedia.tpl.html"
-                    })
-                    .state('Timeline', {
-                        url: '/Timeline',
-                        templateUrl: "src/modules/Timeline/timeline.tpl.html"
                     });
         }]);
 })(window.angular);
