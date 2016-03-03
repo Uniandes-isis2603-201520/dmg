614baa1d3e3ae1d0b85df3656940dd61d3faffc4
JDTawa <jd.torres11@uniandes.edu.co>
2016-03-03 10:11:28 -0500

    Lista de Chequeo implementación servicios REST: Líder de Desarrollo

---
+++ primid/src/main/java/edu/uniandes/dmg/rest/exceptions/PrimidLogicException.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.uniandes.dmg.rest.exceptions;
+
+/*
+ *  Representa las excepciones de la lógica de Primid
+ * @author jd.torres11
+ */
+public class PrimidLogicException extends Exception{
+        /**
+        * versión usada en la serialización de la clase
+         */
+	private static final long serialVersionUID = 1L;
+	/**
+	 * Constructor por defecto
+	 */
+	public PrimidLogicException() {
+	}
+
+	/*
+	 * Constructor con un mensaje
+	 * @param message mensaje de la excepción
+	 */
+	public PrimidLogicException(String message) {
+		super(message);
+	}
+
+        /*
+	 * Constructor con una causa
+	 * @param cause causa de la excepción. Usada para generar la traza.
+	 */
+	public PrimidLogicException(Throwable cause) {
+		super(cause);
+	}
+        /*
+	 * Constructor con mensaje y causa.
+	 * @param message mensaje de la excepción
+	 * @param cause causa de la excepción. Usada para generar la traza.
+	 */
+	public PrimidLogicException(String message, Throwable cause) {
+		super(message, cause);
+	}
+}
---
+++ primid/src/main/java/edu/uniandes/dmg/rest/mocks/ViajeroLogicMock.java
@@ -0,0 +1,10 @@
+
+package edu.uniandes.dmg.rest.mocks;
+
+/*
+ * Objeto de transferencia de datos de Ciudades.
+ * @author Asistente
+ */
+public class ViajeroLogicMock {
+
+}
