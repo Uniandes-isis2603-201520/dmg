214b1f5dc9afc02652b05bd973afd84117518bc4
kl.osorio10@uniandes.edu.co <>
2016-03-15 06:16:55 -0500

    Evento

--- primid/src/main/java/edu/uniandes/dmg/rest/resources/EventoResource.java
+++ primid/src/main/java/edu/uniandes/dmg/rest/resources/EventoResource.java
@@ -6,12 +6,16 @@
 package edu.uniandes.dmg.rest.resources;
 
 import edu.uniandes.dmg.rest.dtos.EventoDTO;
+import edu.uniandes.dmg.rest.exceptions.EventoLogicException;
+import edu.uniandes.dmg.rest.mocks.EventoLogicMock;
 import java.util.List;
+import javax.inject.Inject;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 
 /**
@@ -21,34 +25,67 @@ import javax.ws.rs.Produces;
 @Path ("viajero")
 @Produces("application/json")
 public class EventoResource {
-    
-    //incluir dependencia desde el pomp
 
+    //incluir dependencia desde el pomp
+    @Inject
+    EventoLogicMock eventoLogic;
+
+    	/**
+	 * Obtiene el listado de eventos.
+	 * @return lista de eventos
+	 * @throws EventoLogicException excepción retornada por la lógica
+	 */
      @GET
-    public List<EventoDTO> getEventos()  {
-        return null;
+    public List<EventoDTO> getEventos() throws EventoLogicException {
+        return eventoLogic.getEventos();
     }
-    
+
+        /**
+     * Obtiene un evento
+     * @param id identificador dl evento
+     * @return evento encontrada
+     * @throws EventoLogicException cuando el evento no existe
+     */
          @GET
-    public List<EventoDTO> getEvento(int id)  {
-        return null;
+         @Path("{id: \\d+}")
+    public EventoDTO getEvento(@PathParam("id") Long id) throws EventoLogicException   {
+        return eventoLogic.getEvento(id);
     }
-    
+
+    /**
+     * Agrega un evento
+     * @param evento evento a agregar
+     * @return datos del evento a agregar
+     * @throws EventoLogicException cuando ya existe un evento con el id suministrado
+     */
          @POST
-    public List<EventoDTO> postEvento()  {
-        return null;
+    public EventoDTO createEvento(EventoDTO evento) throws EventoLogicException  {
+        return eventoLogic.createEvento(evento);
     }
-    
-    
+
+        /**
+     * Actualiza los datos de un evento
+     * @param id identificador del evento a modificar
+     * @param evento evento a modificar
+     * @return datos del evento modificada
+     * @throws EventoLogicException cuando no existe un evento con el id suministrado
+     */
          @PUT
-    public List<EventoDTO> putEvento(int id)  {
-        return null;
+         @Path("{id: \\d+}")
+    public EventoDTO updateEvento(@PathParam("id") Long id, EventoDTO evento) throws EventoLogicException {
+        return eventoLogic.updateEvento(id, evento);
     }
-    
-    
+
+    /**
+     * Elimina los datos de un evento
+     * @param id identificador del evento a eliminar
+     * @throws EventoLogicException cuando no existe un evento con el id suministrado
+     */
+
          @DELETE
-    public List<EventoDTO> deleteEvento(int id)  {
-        return null;
+         @Path("{id: \\d+}")
+    public void deleteEvento(@PathParam("id") Long id) throws EventoLogicException  {
+        eventoLogic.deleteEvento(id);
     }
-    
+
 }
