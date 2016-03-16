72b4c5c78fdcd9a10b16cac08cb52081a5bc6f8b
kl.osorio10 <kl.osorio10@172.24.100.224>
2016-03-16 13:54:10 -0500

    Evento - Sitio

--- primid/src/main/webapp/src/modules/evento/evento.svc.js
+++ primid/src/main/webapp/src/modules/evento/evento.svc.js
@@ -1,4 +1,4 @@
-/* 
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -19,6 +19,7 @@
              */
             this.fetchRecords = function () {
                 return $http.get(context);
+
             };
 
             /**
