0a251bcdeda9b6f6df454317b7487f33ab07b8d9
JDTawa <jd.torres11@uniandes.edu.co>
2016-03-16 10:01:36 -0500

    Viajero LOGIC;RESOURCE;DTO

--- primid/src/main/java/edu/uniandes/dmg/rest/mocks/ViajeroLogicMock.java
+++ primid/src/main/java/edu/uniandes/dmg/rest/mocks/ViajeroLogicMock.java
@@ -2,7 +2,10 @@
 package edu.uniandes.dmg.rest.mocks;
 
 import edu.uniandes.dmg.rest.dtos.ViajeroDTO;
+import edu.uniandes.dmg.rest.exceptions.PrimidLogicException;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.enterprise.context.ApplicationScoped;
@@ -17,7 +20,6 @@ import javax.inject.Named;
 @Named
 @ApplicationScoped
 public class ViajeroLogicMock {
-
 
 	// objeto para presentar logs de las operaciones
 	private final static Logger logger = Logger.getLogger(ViajeroLogicMock.class.getName());
@@ -43,4 +45,134 @@ public class ViajeroLogicMock {
     	logger.info("Inicializa la lista de ciudades");
     	logger.info("ciudades" + viajeros );
 }
+
+    /**
+	 * Obtiene el listado de viajeros
+	 * @return lista de viajeros
+	 * @throws PrimidLogicException cuando no existe la lista en memoria
+	 */
+    public List<ViajeroDTO> getViajeros() throws PrimidLogicException {
+    	if (viajeros == null) {
+    		logger.severe("Error interno: lista de viajeros no existe.");
+    		throw new PrimidLogicException("Error interno: lista de viajeros no existe.");
+    	}
+
+    	logger.info("retornando todos los viajeros");
+    	return viajeros;
+    }
+
+      /**
+     * Obtiene un viajero
+     * @param id identificador del viajero
+     * @return viajero encontrado
+     * @throws PrimidLogicException cuando el viajero no existe
+     */
+    public ViajeroDTO getViajeroPorId(Long id) throws PrimidLogicException {
+    	logger.info("recibiendo solicitud del viajero con id " + id);
+
+    	// busca el viajero con el id suministrado
+        for (ViajeroDTO viajero : viajeros) {
+            if (Objects.equals(viajero.getId(), id)){
+            	logger.info("retornando viajero " + viajero);
+                return viajero;
+            }
+        }
+
+        // si no encuentra el viajero
+        logger.severe("No existe viajero con ese id");
+        throw new PrimidLogicException("No existe viajero con ese id");
+    }
+
+     /**
+     * Agrega un Viajero a la lista.
+     * @param newViajero Viajero a adicionar
+     * @throws PrimidLogicException cuando ya existe un Viajero con el id suministrado
+     * @return Viajero agregado
+     */
+    public ViajeroDTO createViajero(ViajeroDTO newViajero) throws PrimidLogicException {
+    	logger.info("recibiendo solicitud de agregar Viajero " + newViajero);
+
+    	// Validacion : el nuevo Viajero tiene id
+    	if ( newViajero.getId() != null ) {
+	    	// busca el viajero con el id suministrado
+	        for (ViajeroDTO viajero : viajeros) {
+	        	// si existe un viajero con ese id
+	            if (Objects.equals(viajero.getId(), newViajero.getId())){
+	            	logger.severe("Ya existe un viajero con ese id");
+	                throw new PrimidLogicException("Ya existe un viajero con ese id");
+	            }
+	        }
+
+	    // Validacion: el nuevo viajero no tiene id
+    	} else {
+
+    		// genera un id para el viajero
+    		logger.info("Generando id para el nuevo viajero");
+    		long newId = 1;
+	        for (ViajeroDTO viajero : viajeros) {
+	            if (newId <= viajero.getId()){
+	                newId =  viajero.getId() + 1;
+	            }
+	        }
+	        newViajero.setId(newId);
+    	}
+
+        // agrega el viajero
+    	logger.info("agregando viajero " + newViajero);
+        viajeros.add(newViajero);
+        return newViajero;
+    }
+
+    /**
+     * Actualiza los datos de un viajero
+     * @param id identificador dl viajero a modificar
+     * @param upViajero viajero a modificar
+     * @return datos del viajero modificado
+     * @throws PrimidLogicException cuando no existe un viajero con el id suministrado
+     */
+    public ViajeroDTO updateViajero(Long id, ViajeroDTO upViajero) throws PrimidLogicException {
+    	logger.info("recibiendo solictud de modificar viajero " + upViajero);
+
+    	// busca el viajero con el id suministrado
+        for (ViajeroDTO viajero : viajeros) {
+            if (Objects.equals(viajero.getId(), id)) {
+
+            	// modifica el viajero
+            	viajero.setId(upViajero.getId());
+                viajero.setName(upViajero.getName());
+
+                // retorna el viajero modificado
+            	logger.info("Modificando viajero " + viajero);
+                return viajero;
+            }
+        }
+
+        // Validacion: no encontró el viajero con ese id
+        logger.severe("No existe un viajero con ese id");
+        throw new PrimidLogicException("No existe un viajero con ese id");
+    }
+
+    /**
+     * Elimina los datos de un viajero
+     * @param id identificador del viajero a eliminar
+     * @throws PrimidLogicException cuando no existe un viajero con el id suministrado
+     */
+    public void deleteViajero(Long id) throws PrimidLogicException {
+    	logger.info("recibiendo solictud de eliminar viajero con id " + id);
+
+    	// busca el viajero con el id suministrado
+        for (ViajeroDTO viajero : viajeros) {
+            if (Objects.equals(viajero.getId(), id)) {
+
+            	// elimina el viajero
+            	logger.info("eliminando viajero " + viajero);
+                viajeros.remove(viajero);
+                return;
+            }
+        }
+
+        // no encontró el viajero con ese id ?
+        logger.severe("No existe un viajero con ese id");
+        throw new PrimidLogicException("No existe un viajero con ese id");
+    }
 }
