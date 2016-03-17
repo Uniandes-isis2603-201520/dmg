2d622e3e92d5eb4408606f6825037847db10bc64
kl.osorio10 <kl.osorio10@172.24.100.224>
2016-03-17 03:49:44 -0500

    Evento REST

---
+++ primid/src/main/java/edu/uniandes/dmg/co/edu/uniandes/rest/cities/dtos/EventoDTO.java
@@ -0,0 +1,135 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.uniandes.dmg.rest.dtos;
+
+/**
+ *
+ * @author karenlorenaosoriohenao
+ */
+public class EventoDTO {
+
+    private Long id;
+    private String name;
+    private String image;
+    private String description;
+    private String dateStarts;
+    private String dateEnds;
+
+    /**
+     * Constructor por defecto
+     */
+    public EventoDTO() {
+	}
+
+    /**
+     * Constructor con parámetros.
+     * @param id identificador del evento
+     * @param name nombre del evento
+     * @param image imagen del evento
+     * @param description decripción del evento
+     */
+    public EventoDTO(Long id, String name, String image, String description, String dateStarts, String dateEnds) {
+		super();
+		this.id = id;
+		this.name = name;
+                this.image = image;
+                this.description = description;
+                this.dateStarts = dateStarts;
+                this.dateEnds = dateEnds;
+	}
+
+	/**
+     * @return the id
+     */
+    public Long getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+        /**
+     * @return the image
+     */
+    public String getImage() {
+        return image;
+    }
+
+    /**
+     * @param image the image to set
+     */
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+        /**
+     * @return the description
+     */
+    public String getDescription() {
+        return name;
+    }
+
+    /**
+     * @param description the description to set
+     */
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+            /**
+     * @return the dateStarts
+     */
+    public String getDateStarts() {
+        return dateStarts;
+    }
+
+    /**
+     * @param dateStarts the dateStarts to set
+     */
+    public void setDateStarts(String dateStarts) {
+        this.dateStarts = dateStarts;
+    }
+
+            /**
+     * @return the dateEnds
+     */
+    public String getDateEnds() {
+        return dateEnds;
+    }
+
+    /**
+     * @param dateEnds the dateEnds to set
+     */
+    public void setDateEnds(String dateEnds) {
+        this.dateEnds = dateEnds;
+    }
+
+    /**
+     * Convierte el objeto a una cadena
+     */
+    @Override
+    public String toString() {
+    	return "{ id : " + getId() + ", name : \"" + getName() + "\" }" ;
+    }
+}
---
+++ primid/src/main/java/edu/uniandes/dmg/co/edu/uniandes/rest/cities/resources/EventoResource.java
@@ -0,0 +1,91 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.rest.cities.resources;
+
+import edu.uniandes.dmg.rest.dtos.EventoDTO;
+import co.edu.uniandes.rest.cities.exceptions.PrimidLogicException;
+import edu.uniandes.dmg.co.edu.uniandes.rest.cities.mocks.EventoLogicMock;
+import java.util.List;
+import javax.inject.Inject;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+
+/**
+ *
+ * @author karenlorenaosoriohenao
+ */
+@Path ("eventos")
+@Produces("application/json")
+public class EventoResource {
+
+    //incluir dependencia desde el pomp
+    @Inject
+    EventoLogicMock eventoLogic;
+
+    	/**
+	 * Obtiene el listado de eventos.
+	 * @return lista de eventos
+	 * @throws PrimidLogicException excepción retornada por la lógica
+	 */
+     @GET
+    public List<EventoDTO> getEventos() throws PrimidLogicException {
+        return eventoLogic.getEventos();
+    }
+
+        /**
+     * Obtiene un evento
+     * @param id identificador dl evento
+     * @return evento encontrada
+     * @throws PrimidLogicException cuando el evento no existe
+     */
+         @GET
+         @Path("{id: \\d+}")
+    public EventoDTO getEvento(@PathParam("id") Long id) throws PrimidLogicException   {
+        return eventoLogic.getEvento(id);
+    }
+
+    /**
+     * Agrega un evento
+     * @param evento evento a agregar
+     * @return datos del evento a agregar
+     * @throws PrimidLogicException cuando ya existe un evento con el id suministrado
+     */
+         @POST
+    public EventoDTO createEvento(EventoDTO evento) throws PrimidLogicException  {
+        return eventoLogic.createEvento(evento);
+    }
+
+        /**
+     * Actualiza los datos de un evento
+     * @param id identificador del evento a modificar
+     * @param evento evento a modificar
+     * @return datos del evento modificada
+     * @throws PrimidLogicException cuando no existe un evento con el id suministrado
+     */
+         @PUT
+         @Path("{id: \\d+}")
+    public EventoDTO updateEvento(@PathParam("id") Long id, EventoDTO evento) throws PrimidLogicException {
+        return eventoLogic.updateEvento(id, evento);
+    }
+
+    /**
+     * Elimina los datos de un evento
+     * @param id identificador del evento a eliminar
+     * @throws PrimidLogicException cuando no existe un evento con el id suministrado
+     */
+
+         @DELETE
+         @Path("{id: \\d+}")
+    public void deleteEvento(@PathParam("id") Long id) throws PrimidLogicException  {
+        eventoLogic.deleteEvento(id);
+    }
+
+}
