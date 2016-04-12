8bc650308325448fdfe18c7a79ff58bb7edb28b2
JDTawa <jd.torres11@uniandes.edu.co>
2016-04-12 10:27:22 -0500

    test Viajero

---
+++ primid-logic/src/test/java/co/edu/uniandes/csw/primid/logic/persistence/ViajeroPersistenceTest.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.primid.logic.persistence;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.runner.RunWith;
+
+/**
+ *
+ * @author jd.torres11
+ */
+@RunWith(Arquillian.class)
+public class ViajeroPersistenceTest {
+
+    public ViajeroPersistenceTest() {
+
+    }
+    @Deployment
+    
+
+    @Test
+    public void testSomeMethod() {
+    }
+
+}
