5994cf608197dc32a3cf72c1f00ac5e25c45e49c
JDTawa <jd.torres11@uniandes.edu.co>
2016-04-12 11:25:01 -0500

    test y xml

--- primid-logic/src/test/java/co/edu/uniandes/csw/primid/logic/persistence/ViajeroPersistenceTest.java
+++ primid-logic/src/test/java/co/edu/uniandes/csw/primid/logic/persistence/ViajeroPersistenceTest.java
@@ -6,13 +6,19 @@
 package co.edu.uniandes.csw.primid.logic.persistence;
 
 import co.edu.uniandes.csw.primid.logic.entities.ViajeroEntity;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import org.junit.Assert;
+
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.junit.Test;
-import static org.junit.Assert.*;
 import org.junit.runner.RunWith;
+import uk.co.jemos.podam.api.PodamFactory;
+import uk.co.jemos.podam.api.PodamFactoryImpl;
 
 /**
  *
@@ -20,6 +26,12 @@ import org.junit.runner.RunWith;
  */
 @RunWith(Arquillian.class)
 public class ViajeroPersistenceTest {
+
+    @Inject
+    private ViajeroPersistence viajeroPersistence;
+    @PersistenceContext
+    private EntityManager em ;
+    private final PodamFactory factory = new PodamFactoryImpl();
 
     public ViajeroPersistenceTest() {
 
@@ -35,7 +47,13 @@ public class ViajeroPersistenceTest {
     }
 
     @Test
-    public void testSomeMethod() {
+    public void createViajeroTest() {
+
+        ViajeroEntity newEntity =factory.manufacturePojo(ViajeroEntity.class);
+
+        ViajeroEntity result = viajeroPersistence.create (newEntity);
+
+        Assert.assertNotNull(result);
     }
 
 }
