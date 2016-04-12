b516be6beac5a34c068e46a57432262ffd5a95e1
JDTawa <jd.torres11@uniandes.edu.co>
2016-04-12 10:40:09 -0500

    test Viajero

--- primid-logic/src/test/java/co/edu/uniandes/csw/primid/logic/persistence/ViajeroPersistenceTest.java
+++ primid-logic/src/test/java/co/edu/uniandes/csw/primid/logic/persistence/ViajeroPersistenceTest.java
@@ -5,8 +5,11 @@
  */
 package co.edu.uniandes.csw.primid.logic.persistence;
 
+import co.edu.uniandes.csw.primid.logic.entities.ViajeroEntity;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.junit.Test;
 import static org.junit.Assert.*;
 import org.junit.runner.RunWith;
@@ -22,7 +25,14 @@ public class ViajeroPersistenceTest {
 
     }
     @Deployment
-  
+    public static JavaArchive createDeployment()
+    {
+        return ShrinkWrap.create(JavaArchive.class)
+                .addPackage(ViajeroEntity.class.getPackage())
+                .addPackage(ViajeroPersistence.class.getPackage())
+                .addAsManifestResource("META-INF/persistence.xml", "persistence.xml")
+                .addAsManifestResource("META-INF/beans.xml", "beans.xml");
+    }
 
     @Test
     public void testSomeMethod() {
