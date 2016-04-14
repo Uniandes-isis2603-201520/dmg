69b8e4cf5470e349f2f5606017f951277b4ec447
la.mesa10 <la.mesa10@172.24.100.220>
2016-04-14 10:56:00 -0500

    Agregada prueba a ItinerarioPersistence

--- primid-logic/src/test/java/co/edu/uniandes/csw/primid/logic/persistence/ItinerarioPersistenceTest.java
+++ primid-logic/src/test/java/co/edu/uniandes/csw/primid/logic/persistence/ItinerarioPersistenceTest.java
@@ -13,11 +13,13 @@ import java.util.List;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.transaction.UserTransaction;
 import junit.framework.Assert;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import uk.co.jemos.podam.api.PodamFactory;
@@ -34,7 +36,8 @@ public class ItinerarioPersistenceTest
     @PersistenceContext
     private EntityManager manejador;
     private final PodamFactory fabrica = new PodamFactoryImpl();
-
+    @Inject
+    UserTransaction ut ;
     private List<ItinerarioEntity> data = new ArrayList<>();
 
 
@@ -46,6 +49,29 @@ public class ItinerarioPersistenceTest
                 .addPackage(ItinerarioPersistence.class.getPackage())
                 .addAsManifestResource("META-INF/persistence.xml","persistence.xml")
                 .addAsManifestResource("META-INF/beans.xml","beans.xml");
+    }
+    @Before
+    public void configTest()
+    {
+        try
+        {
+            ut.begin();
+            clearData();
+            insertData();
+            ut.commit();
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+            try
+            {
+                ut.rollback();
+            }
+            catch(Exception e2)
+            {
+                e2.printStackTrace();
+            }
+        }
     }
     @Test
     public void createItinerarioTest()
@@ -59,6 +85,22 @@ public class ItinerarioPersistenceTest
         Assert.assertEquals(encontrado.getName(),nuevaEntidad.getName());
         Assert.assertEquals(encontrado.getFechaFin(),nuevaEntidad.getFechaFin());
         Assert.assertEquals(encontrado.getFechaInicio(),nuevaEntidad.getFechaInicio());
+
+    }
+    private void clearData()
+    {
+        manejador.createQuery("delete from ItinerarioEntity").executeUpdate();
+    }
+    private void insertData()
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            ItinerarioEntity nuevo = fabrica.manufacturePojo(ItinerarioEntity.class);
+            manejador.persist(nuevo);
+            data.add(nuevo);
+        }
+    }
+    {
 
     }
     @Test
