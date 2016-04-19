503f8dea71ee9892facdd156e03337d77bc555fc
JDTawa <jd.torres11@uniandes.edu.co>
2016-04-19 11:06:02 -0500

    viajero persistenceTest

--- primid-logic/src/test/java/co/edu/uniandes/csw/primid/logic/persistence/ViajeroPersistenceTest.java
+++ primid-logic/src/test/java/co/edu/uniandes/csw/primid/logic/persistence/ViajeroPersistenceTest.java
@@ -6,15 +6,19 @@
 package co.edu.uniandes.csw.primid.logic.persistence;
 
 import co.edu.uniandes.csw.primid.logic.entities.ViajeroEntity;
+import java.util.ArrayList;
+import java.util.List;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.transaction.UserTransaction;
 import org.junit.Assert;
 
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import uk.co.jemos.podam.api.PodamFactory;
@@ -27,16 +31,7 @@ import uk.co.jemos.podam.api.PodamFactoryImpl;
 @RunWith(Arquillian.class)
 public class ViajeroPersistenceTest {
 
-    @Inject
-    private ViajeroPersistence viajeroPersistence;
-    @PersistenceContext
-    private EntityManager em ;
-    private final PodamFactory factory = new PodamFactoryImpl();
-
-    public ViajeroPersistenceTest() {
-
-    }
-    @Deployment
+     @Deployment
     public static JavaArchive createDeployment()
     {
         return ShrinkWrap.create(JavaArchive.class)
@@ -44,6 +39,51 @@ public class ViajeroPersistenceTest {
                 .addPackage(ViajeroPersistence.class.getPackage())
                 .addAsManifestResource("META-INF/persistence.xml", "persistence.xml")
                 .addAsManifestResource("META-INF/beans.xml", "beans.xml");
+    }
+
+    @Inject
+    private ViajeroPersistence viajeroPersistence;
+
+    @PersistenceContext
+    private EntityManager em ;
+
+    @Inject
+    UserTransaction utx;
+
+    private final PodamFactory factory = new PodamFactoryImpl();
+
+    private List<ViajeroEntity> data = new ArrayList<>();
+
+    @Before
+    public void configTest() {
+        try {
+            utx.begin();
+            em.joinTransaction();
+            clearData();
+            insertData();
+            utx.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            try {
+                utx.rollback();
+            } catch (Exception e1) {
+                e1.printStackTrace();
+            }
+        }
+    }
+
+    private void clearData() {
+        em.createQuery("delete from ViajeroEntity").executeUpdate();
+    }
+    private void insertData() {
+        for (int i = 0; i < 3; i++) {
+            ViajeroEntity entity = factory.manufacturePojo(ViajeroEntity.class);
+            em.persist(entity);
+            data.add(entity);
+        }
+    }
+
+    public ViajeroPersistenceTest() {
     }
 
     @Test
@@ -58,6 +98,53 @@ public class ViajeroPersistenceTest {
         Assert.assertEquals(newEntity.getName(), entity.getName());
         Assert.assertEquals(newEntity.getEstado(), entity.getEstado());
         Assert.assertEquals(newEntity.getMail(), entity.getMail());
+    }
+
+    @Test
+    public void getViajerosTest() {
+        List<ViajeroEntity> lista = viajeroPersistence.findAll();
+        Assert.assertEquals(data.size(), lista.size());
+        Assert.assertEquals(data.size(), lista.size());
+        for (ViajeroEntity ent : lista) {
+            boolean found = false;
+            for (ViajeroEntity entity : data) {
+                if (ent.getId().equals(entity.getId())) {
+                    found = true;
+                }
+            }
+            Assert.assertTrue(found);
+        }
+    }
+
+    @Test
+    public void getViajeroTest() {
+        ViajeroEntity entity = data.get(0);
+        ViajeroEntity newEntity = viajeroPersistence.find(entity.getId());
+        Assert.assertNotNull(newEntity);
+        Assert.assertEquals(entity.getName(), newEntity.getName());
+        Assert.assertEquals(entity.getBirthDate(), newEntity.getBirthDate());
+    }
+
+    @Test
+    public void deleteViajeroTest() {
+        ViajeroEntity entity = data.get(0);
+        viajeroPersistence.delete(entity.getId());
+        ViajeroEntity deleted = em.find(ViajeroEntity.class, entity.getId());
+        Assert.assertNull(deleted);
+    }
+
+    @Test
+    public void updateViajeroTest() {
+        ViajeroEntity entity = data.get(0);
+       ViajeroEntity newEntity = factory.manufacturePojo(ViajeroEntity.class);
+
+        newEntity.setId(entity.getId());
+
+        viajeroPersistence.update(newEntity);
+
+        ViajeroEntity resp = em.find(ViajeroEntity.class, entity.getId());
+
+        Assert.assertEquals(newEntity.getName(), resp.getName());
     }
 
 }
